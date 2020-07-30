efdaf6b23bf494139b500b923733ddd5fabf3330
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2020-07-29 19:39:43 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import under_construction from './under_construction.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="uc__wrapper">
+            <div className="uc__details">
+                <h1 className="title">WinEth Coming Soon!</h1>
+                <h3 className="intro">
+                    We are working hard to give you a better experience.
+                </h3>
+                <p className="uc__description">
+                    We are working hard on it & the site may go live very soon. We promise, it will be worth the wait!
+                </p>
+                {/* <div className="uc__subscribe">
+                    <h3>Get Notified When We Go Live</h3>
+                    <div className="uc__form">
+                        <form>                         
+                          <input type="email" className="email" />
+                          <input type="submit" className="submit" />
+                        </form>
+                    </div>
+                </div> */}
+            </div>
+            <div className="uc__art">
+                <img src={under_construction} />
+            </div>
+        </div>
     </div>
   );
 }
